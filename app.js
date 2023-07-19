4a134b8fe99ca943d51a9c2ebc3cccb7200227c0
Aref <Arefkeyhani.pr@gmail.com>
2023-07-19 19:07:49 +0330

    made a ugly todolist project almoust done

--- app.js
+++ app.js
@@ -14,10 +14,24 @@ function addTodo(event) {
   //Create Todo Div
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo-Div");
+  //Append the div to the list of todos
+  todoList.appendChild(todoDiv);
   //Create the LI
   const newTodoLi = document.createElement("li");
   newTodoLi.innerText = todoInput.value;
   newTodoLi.classList.add("todo-item");
   //Append the Li to the Div
   todoDiv.appendChild(newTodoLi);
+  //Completed Button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-ckeck"></i>';
+  completedButton.classList.add("compelete-button");
+  //Append the Completed btn to the Div
+  todoDiv.appendChild(completedButton);
+  //Trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-button");
+  //Append the trash btn to the Div
+  todoDiv.appendChild(trashButton);
 }
