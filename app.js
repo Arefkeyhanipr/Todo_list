a86c1f849b913a2612d51519fba4f4af5a384432
Aref <Arefkeyhani.pr@gmail.com>
2025-02-06 00:23:38 +0330

    now you have to write something in input to add and now the project is responsive for mobile devices

--- app.js
+++ app.js
@@ -13,35 +13,39 @@ document.addEventListener("DOMContentLoaded", getTodos);
 // Function
 
 function addTodo(e) {
-  //Prevent from submiting and reloading the project
-  e.preventDefault();
-  //Create Todo Div
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo-div");
-  //Append the div to the list of todos
-  todoList.appendChild(todoDiv);
-  //Create the LI
-  const newTodoLi = document.createElement("li");
-  newTodoLi.innerText = todoInput.value;
-  newTodoLi.classList.add("todo-item");
-  //Append the Li to the Div
-  todoDiv.appendChild(newTodoLi);
-  //Completed Button
-  const completedButton = document.createElement("button");
-  completedButton.innerHTML = '<i class="fas fa-check"></i>';
-  completedButton.classList.add("complete-button");
-  //Append the Completed btn to the Div
-  todoDiv.appendChild(completedButton);
-  //Trash button
-  const trashButton = document.createElement("button");
-  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add("trash-button");
-  //Append the trash btn to the Div
-  todoDiv.appendChild(trashButton);
-  //Add Todo To Localstorge
-  saveLocalTodos(todoInput.value);
-  //empty the input after adding a todo in the list
-  todoInput.value = "";
+  if (todoInput.value != "") {
+    //Prevent from submiting and reloading the project
+    e.preventDefault();
+    //Create Todo Div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo-div");
+    //Append the div to the list of todos
+    todoList.appendChild(todoDiv);
+    //Create the LI
+    const newTodoLi = document.createElement("li");
+    newTodoLi.innerText = todoInput.value;
+    newTodoLi.classList.add("todo-item");
+    //Append the Li to the Div
+    todoDiv.appendChild(newTodoLi);
+    //Completed Button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-button");
+    //Append the Completed btn to the Div
+    todoDiv.appendChild(completedButton);
+    //Trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-button");
+    //Append the trash btn to the Div
+    todoDiv.appendChild(trashButton);
+    //Add Todo To Localstorge
+    saveLocalTodos(todoInput.value);
+    //empty the input after adding a todo in the list
+    todoInput.value = "";
+  } else {
+    alert("Please Enter what you want to do :)");
+  }
 }
 
 function deleteAndDoneTodo(e) {
